00e304c3e33eb12fbd85a203a8e769dae588ef3c
Mateusz <gorzanski.mateusz@gmail.com>
2018-08-27 22:01:37 +0200

    change 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import Flag from "./../images/143- flag-heart-lgbtq.svg";
 
 const NotFoundPage = () => (
-  <div>
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </div>
+  <section className="section">
+    <div className="container">
+      <h1 className="title is-1">404</h1>
+      <h2 className="subtitle is-2">Page not found</h2>
+    </div>
+  </section>
 )
 
 export default NotFoundPage
